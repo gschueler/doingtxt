b25ff4f3d1b78d9d8e8eff0e0164dee233fbcc01
Greg Schueler <greg.schueler@gmail.com>
2013-05-09 16:37:31 -0700

    Show logs for any entry with same title

--- lib/doing/formatters/status.rb
+++ lib/doing/formatters/status.rb
@@ -4,7 +4,7 @@ module Doing
             attr_reader :output
             def task_out (task)
                 if !task.lines.nil?
-                    @output<<"---\n"
+                    # @output<<"---\n"
                     @output<<task.lines.join("\n")
                     @output<<"\n"
                 end
@@ -20,7 +20,17 @@ module Doing
                 else
                     @output<<"Last worked on: #{tasks[-1].title}\n"
                 end
-                task_out(tasks[-1])
+                title=tasks[-1].title
+                #look for previous logs for same task
+                found=tasks[0..-2].find_all { |e| e.title==title }
+                if found.size > 0
+                    found.each do |i|
+                        task_out(i)
+                    end
+                end
+                if tasks[-1].lines.size>0 
+                    task_out(tasks[-1])
+                end
             end
         end
     end
